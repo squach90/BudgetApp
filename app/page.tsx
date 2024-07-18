72d412d7fde274a0577793e19ec0d92d45d96140
squach90 <160175686+squach90@users.noreply.github.com>
2024-07-18 17:44:27 +0200

    Add title and icon

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
 import Image from "next/image";
 import Home from "./Home/page";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Budget Manager",
+  description: "Simple Budget Manager build in 4h",
+  icons: {
+    icon: "/public/logo.png",
+  },
+};
 
 export default function Main() {
   return (
